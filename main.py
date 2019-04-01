713aa8a400a0f874551982459e9ecd7e9ed3dac8
Quanlai Li <quanlai@uber.com>
2019-04-01 12:06:00 -0700

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from googletrans import Translator
+import sys
+
+print "Please paste Spanish lyrics and then press Alt+D:"
+lyrics = sys.stdin.readlines()
+translator = Translator()
+for line in lyrics:
+    trans = translator.translate(line, src='es', dest='en')
+    if line != '\n':
+        print line.replace('\n', '')
+        print trans.text
+    else:
+        print '\n'
