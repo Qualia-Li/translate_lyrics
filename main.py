5b0ecc8a0755b3875dc2ddd1c07bc93fa5d3dafa
Quanlai Li <qli@lyft.com>
2020-06-17 14:05:28 -0700

    Update to Python3

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from googletrans import Translator
 import sys
 
-print "Please paste Spanish lyrics and then press Alt+D:"
+print("Please paste Spanish lyrics and then press Alt+D:")
 lyrics = sys.stdin.readlines()
 translator = Translator()
 for line in lyrics:
     trans = translator.translate(line, dest='en')
     if line != '\n':
-        print line.replace('\n', '')
-        print trans.text
+        print(line.replace('\n', ''))
+        print(trans.text)
     else:
-        print '\n'
+        print('\n')
